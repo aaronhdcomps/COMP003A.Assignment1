407ac86290ba5d91db49bba9fadc4e124387a72b
Aaron Abramson <aaronabramson@my.whccd.edu>
2023-01-18 19:58:47 -0800

    Updated code.

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -1,10 +1,118 @@
-﻿namespace COMP003A.Assignment1
+﻿//  This is a single-line comment
+/*  This is a multi-line comment
+ *  
+ *  Author: Aaron Abramson
+ *  Course: COMP-003A
+ *  Purpose: Code for Assignment 1
+ */
+namespace COMP003A.Assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("100 things about me");
+            Console.WriteLine("1. My first name is Aaron");
+            Console.WriteLine("2. My last name is Abramson");
+            Console.WriteLine("3. My favorite color is blue");
+            Console.WriteLine("4. My major is in Computer Science");
+            Console.WriteLine("5. I previously worked in the IT field for 10 years");
+            Console.WriteLine("6. I once had an onsite client walk away from a job site in my shoes");
+            Console.WriteLine("7. I have a custom built PC.");
+            Console.WriteLine("8. I enjoy watching k-dramas with my girlfriend.");
+            Console.WriteLine("9. I enjoy video games.");
+            Console.WriteLine("10. I hope to become a Software Engineer.");
+            Console.WriteLine("11. Details");
+            Console.WriteLine("12. Details");
+            Console.WriteLine("13. Details");
+            Console.WriteLine("14. Details");
+            Console.WriteLine("15. Details");
+            Console.WriteLine("16. Details");
+            Console.WriteLine("17. Details");
+            Console.WriteLine("18. Details");
+            Console.WriteLine("19. Details");
+            Console.WriteLine("20. Details");
+            Console.WriteLine("21. Details");
+            Console.WriteLine("22. Details");
+            Console.WriteLine("23. Details");
+            Console.WriteLine("24. Details");
+            Console.WriteLine("25. Details");
+            Console.WriteLine("26. Details");
+            Console.WriteLine("27. Details");
+            Console.WriteLine("28. Details");
+            Console.WriteLine("29. Details");
+            Console.WriteLine("30. Details");
+            Console.WriteLine("31. Details");
+            Console.WriteLine("32. Details");
+            Console.WriteLine("33. Details");
+            Console.WriteLine("34. Details");
+            Console.WriteLine("35. Details");
+            Console.WriteLine("36. Details");
+            Console.WriteLine("37. Details");
+            Console.WriteLine("38. Details");
+            Console.WriteLine("39. Details");
+            Console.WriteLine("40. Details");
+            Console.WriteLine("41. Details");
+            Console.WriteLine("42. Details");
+            Console.WriteLine("43. Details");
+            Console.WriteLine("44. Details");
+            Console.WriteLine("45. Details");
+            Console.WriteLine("46. Details");
+            Console.WriteLine("47. Details");
+            Console.WriteLine("48. Details");
+            Console.WriteLine("49. Details");
+            Console.WriteLine("50. Details");
+            Console.WriteLine("51. Details");
+            Console.WriteLine("52. Details");
+            Console.WriteLine("53. Details");
+            Console.WriteLine("54. Details");
+            Console.WriteLine("55. Details");
+            Console.WriteLine("56. Details");
+            Console.WriteLine("57. Details");
+            Console.WriteLine("58. Details");
+            Console.WriteLine("59. Details");
+            Console.WriteLine("60. Details");
+            Console.WriteLine("61. Details");
+            Console.WriteLine("62. Details");
+            Console.WriteLine("63. Details");
+            Console.WriteLine("64. Details");
+            Console.WriteLine("65. Details");
+            Console.WriteLine("66. Details");
+            Console.WriteLine("67. Details");
+            Console.WriteLine("68. Details");
+            Console.WriteLine("69. Details");
+            Console.WriteLine("70. Details");
+            Console.WriteLine("71. Details");
+            Console.WriteLine("72. Details");
+            Console.WriteLine("73. Details");
+            Console.WriteLine("74. Details");
+            Console.WriteLine("75. Details");
+            Console.WriteLine("76. Details");
+            Console.WriteLine("77. Details");
+            Console.WriteLine("78. Details");
+            Console.WriteLine("79. Details");
+            Console.WriteLine("80. Details");
+            Console.WriteLine("81. Details");
+            Console.WriteLine("82. Details");
+            Console.WriteLine("83. Details");
+            Console.WriteLine("84. Details");
+            Console.WriteLine("85. Details");
+            Console.WriteLine("86. Details");
+            Console.WriteLine("87. Details");
+            Console.WriteLine("88. Details");
+            Console.WriteLine("89. Details");
+            Console.WriteLine("90. Details");
+            Console.WriteLine("91. Details");
+            Console.WriteLine("92. Details");
+            Console.WriteLine("93. Details");
+            Console.WriteLine("94. Details");
+            Console.WriteLine("95. Details");
+            Console.WriteLine("96. Details");
+            Console.WriteLine("97. Details");
+            Console.WriteLine("98. Details");
+            Console.WriteLine("99. Details");
+            Console.WriteLine("100. Details");
+
         }
     }
 }
